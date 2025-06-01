7056666316613db4ce49d6cdb257604eb3024de3
Samuel Rosero <saferoxx215@gmail.com>
2025-06-01 13:44:26 -0500

    agregar doctor dasshboard con botones funcionales, cerrar sesion y navbar

---
+++ src/intefaces/User.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+  _id: string;
+  name: string;
+  phone: string;
+  email: string;
+  role: string;
+}
+
--- src/routes/DoctorRoutes.tsx
+++ src/routes/DoctorRoutes.tsx
@@ -1,9 +1,10 @@
 import { Route } from 'react-router-dom';
 import { ProtectedRoute } from '../middleware/authMiddleware';
+import DoctorDashboard from '../pages/doctor/DoctorDashboard';
 
-// Aquí deberás importar tus componentes reales
-const DoctorDashboard = () => <div>Doctor Dashboard</div>;
 const PatientList = () => <div>Patient List</div>;
+const BankQuestions = () => <div>Bank Questions</div>;
+const CreatePatient = () => <div>Create Patient</div>;
 
 export const DoctorRoutes = (
   <>
@@ -20,6 +21,22 @@ export const DoctorRoutes = (
       element={
         <ProtectedRoute allowedRole={['admin', 'doctor']}>
           <PatientList />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/doctor/bank-questions"
+      element={
+        <ProtectedRoute allowedRole={['admin', 'doctor']}>
+          <BankQuestions />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/doctor/create-patient"
+      element={
+        <ProtectedRoute allowedRole={['admin', 'doctor']}>
+          <CreatePatient />
         </ProtectedRoute>
       }
     />
